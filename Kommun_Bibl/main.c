5b12fa958eeed71e791d940f299e36a91dbe6000
PavelSchal <47889973+PavelSchal@users.noreply.github.com>
2019-02-22 13:20:52 +0100

    Test

---
+++ Kommun_Bibl/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include "Bibl.h"
+#include "testHeader.h"
+
+int main(void) {
+    printf("Hello world!\n");
+
+    while(1) {
+       stateMachine();
+    }
+
+    return 0;
+}
